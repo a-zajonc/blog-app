4d63e679007ba1421aaba8bb1b29e315988c3146
Ada <byjean94@gmail.com>
2022-06-28 14:44:22 +0200

    Upgraded Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import * as React from 'react'
-import styles from './App.module.css';
+import * as React from 'react';
 import { Navigation } from './components/Navigation';
 import { Footer } from './components/Footer';
 import {
@@ -10,28 +9,28 @@ import { ArticlePage } from './pages/ArticlePage';
 import { ArticlesList } from './pages/ArticlesList';
 import { Contact } from './pages/Contact.js';
 import { Home } from './pages/Home.js';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function App() {
 
   return (
-    <Router>
-      <div className={styles.App}>
-        <Navigation imgSrc="https://iconarchive.com/download/i86440/martin-berube/flat-animal/rabbit.ico" alt="Logo z Królikiem" />
-        <Routes>
-          <Route exact path='/' element={< Home />}></Route>
-          <Route exact path='/about' element={< About />}></Route>
-          <Route exact path='/articles' element={< ArticlesList />}></Route>
-          <Route exact path='/contact' element={< Contact />}></Route>
-          <Route exact path='articles/:slug' element={< ArticlePage />}></Route>
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <div>
+          <Navigation imgSrc="https://whiterabbit.group/images/white-rabbit-logo-black.jpg" alt="Logo z Królikiem" />
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/about' element={< About />}></Route>
+            <Route exact path='/articles' element={< ArticlesList />}></Route>
+            <Route exact path='/contact' element={< Contact />}></Route>
+            <Route exact path='articles/:slug' element={< ArticlePage />}></Route>
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </ChakraProvider>
   )
 }
 
 export default App;
-
-
-// {data ? <Article imgSrc={data.imgSrc} alt={data.alt} title={data.title} author={data.author} content={data.content} /> : <Spinner animation="border" variant="primary" className={styles.spinner} />
-// }
